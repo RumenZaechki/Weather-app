0619675ccd2795c98b6d3245c6b94d7519cecad4
Rumen Zaechki <73949282+RumenZaechki@users.noreply.github.com>
2021-12-22 09:40:20 +0200

    removed TempMax and TempMIn functions

--- src/temps.js
+++ src/temps.js
@@ -6,14 +6,14 @@ export function FeelsLike(temp) {
     const feelsLike = temp['feels_like'];
     document.getElementById('feelsLike').innerHTML = 'Feels like:' + Math.round(feelsLike) + '°C';
 }
-export function TempMax(temp) {
-    const max = temp["temp_max"];
-    document.getElementById('temperatureMax').innerHTML = '| Max:' + Math.round(max) + '°C';
-}
-export function TempMin(temp) {
-    const min = temp["temp_min"];
-    document.getElementById('temperatureMin').innerHTML = 'Min:' + Math.round(min) + '°C';
-}
+// export function TempMax(temp) {
+//     const max = temp["temp_max"];
+//     document.getElementById('temperatureMax').innerHTML = '| Max:' + Math.round(max) + '°C';
+// }
+// export function TempMin(temp) {
+//     const min = temp["temp_min"];
+//     document.getElementById('temperatureMin').innerHTML = 'Min:' + Math.round(min) + '°C';
+// }
 export function Pressure(temp) {
     const pressure = temp["pressure"];
     document.getElementById('pressure').innerHTML = 'Pressure: ' + Math.round(pressure) + 'hPa';
